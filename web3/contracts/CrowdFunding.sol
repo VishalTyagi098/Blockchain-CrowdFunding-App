8dcc5e3e12192e6497add474bfc8c63c3082266b
Vishal <vishaltyagi.dev098@gmail.com>
2023-06-19 16:28:40 +0530

    started smart contract

---
+++ web3/contracts/CrowdFunding.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract CrowdFunding {
+    struct Campaign {
+        address owner;
+        string title;
+        string description;
+        uint256 target;
+        uint256 deadline;
+        uint256 amountCollected;
+        string image;
+        address[] donators;
+        uint256[] donations;
+    }
+
+    mapping(uint256 => Campaign) public campaigns;
+
+    uint256 public numberOfCampaigns = 0;
+
+    // FUNCTIONS
+
+    // 1.
+    function createCampaign() {}
+
+    // 2.
+    function donateToCampaign() {}
+
+    // 3.
+    function getDonators() {}
+
+    // 4.
+    function getCampaigns() {}
+}
