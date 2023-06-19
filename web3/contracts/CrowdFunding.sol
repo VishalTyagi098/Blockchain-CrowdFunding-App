baf63675dac87f9b1ae4bdb5b65c0c14c0ac4ad1
Vishal <vishaltyagi.dev098@gmail.com>
2023-06-19 16:45:39 +0530

    donateToCampaign

--- web3/contracts/CrowdFunding.sol
+++ web3/contracts/CrowdFunding.sol
@@ -50,7 +50,20 @@ contract CrowdFunding {
     }
 
     // 2.
-    function donateToCampaign() {}
+    function donateToCampaign(uint256 _id) public payable {
+        uint256 amount = msg.value;
+
+        Campaign storage campaign = campaigns[_id];
+
+        campaign.donators.push(msg.sender);
+        campaign.donations.push(amount);
+
+        (bool sent,)= payable(campaign.owner).call{value:amount}{""};
+
+        if(sent){
+            campaign.amountCollected=campaign.amountCollected+amount;
+        }
+    }
 
     // 3.
     function getDonators() {}
