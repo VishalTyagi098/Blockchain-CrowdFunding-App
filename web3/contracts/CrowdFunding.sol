962a1231f83128c59472f0a7a8430cf2ed28a72b
Vishal <vishaltyagi.dev098@gmail.com>
2023-06-19 16:53:31 +0530

    getCampaigns()

--- web3/contracts/CrowdFunding.sol
+++ web3/contracts/CrowdFunding.sol
@@ -71,5 +71,14 @@ contract CrowdFunding {
     }
 
     // 4.
-    function getCampaigns() {}
+    function getCampaigns() public view returns (Campaign[] memory) {
+        Campaign memory allCampaigns=new Campaign[](numberOfCampaigns);
+
+        for(uint i=0;i<numberOfCampaigns;i++){
+            Campaign storage item= campaigns[i];
+            allCampaigns[i]=item;
+        }
+
+        return allCampaigns;
+    }
 }
