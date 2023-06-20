77771623078eea36f26612e4737c7dbd3ec530cf
Vishal <vishaltyagi.dev098@gmail.com>
2023-06-20 12:37:08 +0530

    app incomplete

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 
+import "./index.css";
 import App from "./App.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
