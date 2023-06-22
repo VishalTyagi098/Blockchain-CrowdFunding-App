925c60568623dc99b2d3d066a7f6beeafa52e9aa
Vishal <vishaltyagi.dev098@gmail.com>
2023-06-22 13:53:08 +0530

    formfield done

--- client/src/pages/CreateCampaign.jsx
+++ client/src/pages/CreateCampaign.jsx
@@ -2,12 +2,59 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { ethers } from "ethers";
 
+// INTERNAL IMPORT
+import { FormField } from "../components";
 import { money } from "../assets";
 import { CustomButton } from "../components";
 import { checkIfImage } from "../utils/utils";
 
 const CreateCampaign = () => {
-  return <div>CreateCampaign</div>;
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const [form, setForm] = useState({
+    name: "",
+    title: "",
+    description: "",
+    target: "",
+    deadline: "",
+    image: "",
+  });
+
+  const handleSubmit = () => {};
+
+  return (
+    <div className="bg-[#1c1c24] flex justify-center items-center flex-col rounded-[10px] sm:p-10 p-4">
+      {isLoading && "Loader..."}
+      <div className="flex justify-center items-center p-[16px] sm:min-w-[380px] bg-[#3a3a43] rounded-[10px]">
+        <h1 className="font-epilogue font-bold sm:text-[25px] text-[18px] leading-[38px] text-white">
+          Start a campaign
+        </h1>
+      </div>
+
+      <form
+        action=""
+        onSubmit={handleSubmit}
+        className="w-full mt-[65px] flex flex-col gap-[30px]"
+      >
+        <div className="flex flex-wrap gap-[40px]">
+          <FormField
+            labelName="Your Name"
+            placeholder="john Doe"
+            inputType="Text"
+            value={form.name}
+            handleChange={() => {}}
+          />
+          <FormField
+            labelName="Campaign Title"
+            placeholder="Write a title"
+            inputType="Text"
+            value={form.title}
+            handleChange={() => {}}
+          />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default CreateCampaign;
